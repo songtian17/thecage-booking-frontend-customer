7815efa37bc052e2c2e04d7aee1dadd279a18a89
songtian17 <liust.17@ichat.sp.edu.sg>
2019-11-25 16:27:36 +0800

    init

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,27 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/html-closing-bracket-spacing': ['error', {
+      startTag: 'never',
+      endTag: 'never',
+      selfClosingTag: 'always',
+    }],
+    'vue/html-quotes': ['error', 'double'],
+    'vue/attributes-order': ['error', {
+      order: [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'GLOBAL',
+        'UNIQUE',
+        'TWO_WAY_BINDING',
+        'OTHER_DIRECTIVES',
+        'OTHER_ATTR',
+        'EVENTS',
+        'CONTENT',
+      ],
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
