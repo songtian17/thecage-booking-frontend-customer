bf95c19498b609a599ec11136a2a111dfcff0423
songtian17 <liust.17@ichat.sp.edu.sg>
2019-11-28 11:52:12 +0800

    Add vuetify and global scss

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <navbar></navbar>
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+  </v-app>
 </template>
 
+<script>
+import Navbar from '@/components/Navbar.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    Navbar,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+@import url('https://fonts.googleapis.com/css?family=Montserrat:500,700&display=swap');
 </style>
