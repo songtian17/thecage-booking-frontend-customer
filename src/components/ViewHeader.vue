928ee54c1899fd7bb2b2c4977c0e4589eebce30f
songtian17 <liust.17@ichat.sp.edu.sg>
2019-12-03 12:03:41 +0800

    Add header

---
+++ src/components/ViewHeader.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="header">
+    <p>
+      THE CAGE <br />
+      BOOKING SYSTEM.
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ViewHeader',
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  background-color: $primary;
+
+  p {
+    padding: 40px 24px;
+    font-family: "Montserrat", sans-serif;
+    font-size: 23px;
+    font-weight: 700;
+    color: white;
+  }
+}
+</style>
