d54a18166032f446aac442544fd37e9daa462161
songtian17 <liust.17@ichat.sp.edu.sg>
2019-12-27 15:52:36 +0800

    Add vuelidate dependency

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import axios from 'axios';
 import App from './App.vue';
 import './registerServiceWorker';
@@ -8,6 +9,7 @@ import store from './store';
 
 Vue.config.productionTip = false;
 Vue.prototype.$axios = axios;
+Vue.use(Vuelidate);
 
 new Vue({
   router,
