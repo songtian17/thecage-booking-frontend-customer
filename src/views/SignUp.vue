a428f9cc6bb6ca7881884c89009da67b5e252aa7
songtian17 <liust.17@ichat.sp.edu.sg>
2019-12-27 17:09:27 +0800

    Implement input validation for sign up

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -7,26 +7,95 @@
     <div class="container">
       <div class="header">Sign Up</div>
       <form @submit.prevent="submitForm">
-        <label for="email">Email</label>
-        <br />
-        <input id="email" v-model="email" type="email" name="email" />
-        <br />
-        <label for="name">Name</label>
-        <br />
-        <input id="name" v-model="name" type="text" name="name" />
-        <br />
-        <label for="phone">Phone Number</label>
-        <br />
-        <input id="phone" v-model="phone" type="tel" name="phone" />
-        <br />
-        <label for="password">Password</label>
-        <br />
-        <input id="password" v-model="password" type="password" name="password" />
-        <br />
-        <label for="confirm-pass">Confirm Password</label>
-        <br />
-        <input id="confirm-pass" v-model="confirmPassword" type="password" name="confirm-pass" />
-        <br />
+        <div class="form-group" :class="{ 'form-group--error': $v.formData.email.$error }">
+          <label class="form__label" for="email">Email</label>
+          <input
+            id="email"
+            v-model.lazy.trim="formData.email"
+            class="form__input"
+            type="email"
+            @change="$v.formData.email.$touch"
+          />
+          <div v-if="$v.formData.email.$error">
+            <div v-if="!$v.formData.email.required" class="form__error">
+              Field is required.
+            </div>
+            <div v-if="!$v.formData.email.email" class="form__error">
+              Enter a valid email address.
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group" :class="{ 'form-group--error': $v.formData.name.$error }">
+          <label class="form__label" for="name">Name</label>
+          <input
+            id="name"
+            v-model.lazy.trim="formData.name"
+            class="form__input"
+            type="name"
+            @change="$v.formData.name.$touch"
+          />
+          <div v-if="$v.formData.name.$error">
+            <div v-if="!$v.formData.name.required" class="form__error">
+              Field is required.
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group" :class="{ 'form-group--error': $v.formData.phone.$error }">
+          <label class="form__label" for="phone">Phone Number</label>
+          <input
+            id="phone"
+            v-model.lazy.trim="formData.phone"
+            class="form__input"
+            type="phone"
+            @change="$v.formData.phone.$touch"
+          />
+          <div v-if="$v.formData.phone.$error">
+            <div v-if="!$v.formData.phone.required" class="form__error">
+              Field is required.
+            </div>
+            <div v-if="!$v.formData.phone.phoneNumber" class="form__error">
+              Enter a valid phone number.
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group" :class="{ 'form-group--error': $v.formData.password.$error }">
+          <label class="form__label" for="password">Password</label>
+          <input
+            id="password"
+            v-model.lazy="formData.password"
+            class="form__input"
+            type="password"
+            @change="$v.formData.password.$touch"
+          />
+          <div v-if="$v.formData.password.$error">
+            <div v-if="!$v.formData.password.required" class="form__error">
+              Field is required.
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group" :class="{ 'form-group--error': $v.formData.password2.$error }">
+          <label class="form__label" for="password2">Confirm Password</label>
+          <input
+            id="password2"
+            v-model.lazy="formData.password2"
+            class="form__input"
+            type="password"
+            @change="$v.formData.password2.$touch"
+          />
+          <div v-if="$v.formData.password2.$error">
+            <div v-if="!$v.formData.password2.required" class="form__error">
+              Field is required.
+            </div>
+            <div v-if="!$v.formData.password2.sameAsPassword" class="form__error">
+              Passwords must match
+            </div>
+          </div>
+        </div>
+
         <div class="actions">
           <input id="submit" type="submit" value="Sign Up" />
         </div>
@@ -36,43 +105,75 @@
 </template>
 
 <script>
+import { required, email, sameAs } from 'vuelidate/lib/validators';
 import ViewHeader from '@/components/ViewHeader.vue';
+
+const phoneNumber = value => /^$|^[8-9][0-9]{7}$/.test(value);
 
 export default {
   name: 'SignUp',
   data() {
     return {
-      email: '',
-      name: '',
-      phone: '',
-      password: '',
-      confirmPassword: '',
+      formData: {
+        email: '',
+        name: '',
+        phone: '',
+        password: '',
+        password2: '',
+      },
     };
+  },
+  validations: {
+    formData: {
+      email: {
+        required,
+        email,
+      },
+      name: {
+        required,
+      },
+      phone: {
+        required,
+        phoneNumber,
+      },
+      password: {
+        required,
+      },
+      password2: {
+        required,
+        sameAsPassword: sameAs('password'),
+      },
+    },
   },
   components: {
     ViewHeader,
   },
   methods: {
     submitForm() {
-      this.$axios
-        .post(`${process.env.VUE_APP_API}/signup`, {
-          email: this.email,
-          username: this.name,
-          phone: this.phone,
-          password: this.password,
-        })
-        .then(() => {
-          this.$router.push('/');
-        })
-        .catch((err) => {
-          console.log(err);
-        });
+      this.$v.$touch();
+      if (!this.$v.$invalid) {
+        this.$axios
+          .post(`${process.env.VUE_APP_API}/signup`, {
+            email: this.email,
+            username: this.name,
+            phone: this.phone,
+            password: this.password,
+          })
+          .then(() => {
+            this.$router.push('/');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
+@include form-group;
+
 .container {
   @media screen and (max-width: 400px) {
     width: 100%;
@@ -103,34 +204,26 @@ export default {
     padding: 16px;
     @include montserrat($h3, 600);
   }
+}
 
-  form {
-    padding: 16px;
+form {
+  padding: 16px;
 
-    label {
-      @include montserrat($h5, 400);
-    }
+  .form__input {
+    width: 100%;
+  }
 
-    input:not(#submit) {
-      border: 1px solid $secondary;
-      margin-top: 4px;
-      margin-bottom: 12px;
-      width: 100%;
-      padding: 4px 6px;
-    }
+  .actions {
+    overflow: auto;
 
-    .actions {
-      overflow: auto;
-
-      #submit {
-        background-color: $primary;
-        color: white;
-        padding: 6px 12px;
-        border-radius: 4px;
-        float: right;
-        @include montserrat($h4, 500);
-        cursor: pointer;
-      }
+    #submit {
+      background-color: $primary;
+      color: white;
+      padding: 6px 12px;
+      border-radius: 4px;
+      float: right;
+      @include montserrat($h4, 500);
+      cursor: pointer;
     }
   }
 }
